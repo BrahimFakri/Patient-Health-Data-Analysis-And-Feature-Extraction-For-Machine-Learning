8391360c826df016060b1850786308fcb21d3feb
BrahimFakri <119059452+BrahimFakri@users.noreply.github.com>
2023-04-18 12:39:53 -0400

    Add files via upload

---
+++ src/utils/extract_vision_features.py
@@ -0,0 +1,38 @@
+def extract_vision_features(img):
+
+  #Initialize the densefeature_embeddings with an empty list:
+  densefeature_embeddings = []
+
+  #Initialize the prediction_embeddings with an empty list:
+  prediction_embeddings = []
+
+  # Select model 
+  model = xrv.models.DenseNet(weights = "densenet121-res224-chex")
+
+  #process image
+  img = xrv.datasets.normalize(img, 255)
+  img = cv2.resize(img, (224, 224), interpolation = cv2.INTER_AREA)   
+  img = img[None, :, :]
+  
+  with torch.no_grad():
+    img = torch.from_numpy(img).unsqueeze(0)
+          
+    # Extract dense features
+    feats = model.features(img)
+    feats = F.relu(feats, inplace=True)
+    feats = F.adaptive_avg_pool2d(feats, (1, 1))
+    densefeatures = feats.cpu().detach().numpy().reshape(-1)
+    densefeature_embeddings = densefeatures
+
+    # Extract predicted probabilities of considered 18 classes:
+    preds = model(img).cpu()
+    predictions = preds[0].detach().numpy()
+    prediction_embeddings = predictions    
+
+    df_vision_dense_embeddings_fusion = pd.DataFrame(densefeature_embeddings.reshape(1,-1), columns=['vd_'+str(i) for i in range(densefeature_embeddings.shape[0])])
+
+
+    df_vision_predictions_embeddings_fusion = pd.DataFrame(prediction_embeddings.reshape(1,-1), columns=['vp_'+str(i) for i in range(prediction_embeddings.shape[0])])
+
+
+    return df_vision_predictions_embeddings_fusion , df_vision_dense_embeddings_fusion
